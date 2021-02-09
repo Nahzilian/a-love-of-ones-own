234bfd62b54d8121f1bfa72bf46cf58994d28bba
Duc Nguyen <nahz3112@gmail.com>
2021-02-09 11:46:33 -0500

    First draft for quiz

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
+function getPos(el) {
+    for (var lx = 0, ly = 0;
+        el != null;
+        lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
+    return { left: lx, top: ly , behavior: "smooth"};
+}
+
 function evaluateForm(e) {
     e.preventDefault();
     var q1 = document.getElementsByName('question1');
@@ -8,10 +15,21 @@ function evaluateForm(e) {
     var score = 0;
     var answers = [q1, q2, q3, q4, q5];
     answers.forEach(x => {
-        for(var i = 0; i< x.length; i++){
-            if(x[i].checked) score+= parseInt(x[i].value);
+        for (var i = 0; i < x.length; i++) {
+            if (x[i].checked) 
+                score += parseInt(x[i].value);
+            else x[i].disabled = true;
         }
     })
-    document.getElementById("score").innerHTML = score
-    console.log(score)
+    document.getElementById("score").innerHTML = score;
+    var explanations = document.getElementsByClassName('explanations');
+    var scoreWrapper = document.getElementById('score-wrapper');
+    var pos = getPos(scoreWrapper)
+    for (var i in explanations) {
+        explanations[i].className += ' show-exp';
+    }
+    window.scrollTo(pos);
+    scoreWrapper.style.display = 'block';
+    document.getElementById("retry").style.display = 'block';
+    document.getElementById("eval").style.display = 'none';
 }
